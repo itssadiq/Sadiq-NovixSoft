f1696cf531f07a65df98d8646e7cac9e66b98ed0
itssadiq <sadiqalishaikh009@gmail.com>
2026-02-08 17:37:46 +0500

    About section on homepage.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/home/Hero";
 import FeaturedWork from "../components/home/FeaturedWork"; // Import the section
+import About from "@/components/home/About";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-10 md:gap-20">
       <Hero />
       <FeaturedWork />
+      <About />
 
       {/* Other sections... */}
       <div className="h-20"></div>
